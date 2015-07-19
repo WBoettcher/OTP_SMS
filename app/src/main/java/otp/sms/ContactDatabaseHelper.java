660f919f4b67d525eb6cf8396061ba0480096c70
Andrew Harris <aharris1@stevens.edu>
2015-07-18 19:35:34 -0700

    ContactInterface Activity demos database components of the app.  It allows the user to add contacts, refresh the listview to see all new contacts, and reset the contacts table (mostly for debugging purposes).

--- app/src/main/java/otp/sms/ContactDatabaseHelper.java
+++ app/src/main/java/otp/sms/ContactDatabaseHelper.java
@@ -2,6 +2,7 @@ package otp.sms;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -10,25 +11,21 @@ import android.util.Log;
  * Created by aharris on 7/16/15.
  */
 public class ContactDatabaseHelper extends SQLiteOpenHelper {
-
-
-    //TAG for Logcat.
-    private final static String TAG = "ContactDatabaseHelper";
-    //DB Version
-    private static final int DATABASE_VERSION = 1;
-
-    //DB Name
-    private static final String DATABASE_NAME = "contactsDatabase";
+//TODO: Make this class a singleton to avoid slowing down the UI thread.
 
     //Table Names
-    private static final String TABLE_CONTACTS = "contacts";
-
+    public static final String TABLE_CONTACTS = "contacts";
     //Column names
-    public static final String KEY_ID = "id";
+    public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
     public static final String KEY_PHONENUMBER = "phonenumber";
     public static final String KEY_CODE = "code";
-
+    //TAG for Logcat.
+    private final static String TAG = "ContactDatabaseHelper";
+    //DB Version
+    private static final int DATABASE_VERSION = 2; //incremented during testing
+    //DB Name
+    private static final String DATABASE_NAME = "contactsDatabase";
     private static final String CREATE_TABLE_CONTACTS = String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY, %s TEXT, %s TEXT, %s TEXT)", TABLE_CONTACTS, KEY_ID, KEY_NAME, KEY_PHONENUMBER, KEY_CODE);
 
     public ContactDatabaseHelper(Context context){
@@ -67,5 +64,15 @@ public class ContactDatabaseHelper extends SQLiteOpenHelper {
         db.close(); //closes the connection to the database
     }
 
-    //TODO Implement a read method to pull data from the database and return a cursor
+    public Cursor getContactsCursor() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = {KEY_ID, KEY_NAME, KEY_PHONENUMBER, KEY_CODE};
+        return db.query(TABLE_CONTACTS, columns, null, null, null, null, null, null);
+    }
+
+    public void deleteTable() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(db);
+    }
 }
--- app/src/main/java/otp/sms/ContactInterface.java
+++ app/src/main/java/otp/sms/ContactInterface.java
@@ -1,11 +1,13 @@
 package otp.sms;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 /**
  * CURRENTLY IN DEVELOPMENT
@@ -22,25 +24,59 @@ public class ContactInterface extends Activity {
     private EditText phoneNumber;
     private EditText code;
     private ListView contactsList;
+    private SimpleCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_interface);
+
         contact_db = new ContactDatabaseHelper(getApplicationContext());
         Log.d(TAG, "Contact Database Helper constructed");
+
         name = (EditText) findViewById(R.id.c_name);
         phoneNumber = (EditText) findViewById(R.id.c_phoneNumber);
         code = (EditText) findViewById(R.id.c_code);
+
+        contactsList = (ListView) findViewById(R.id.contactsListView);
+        Cursor contactsCursor;
+        contactsCursor = contact_db.getContactsCursor();
+        String[] from = {ContactDatabaseHelper.KEY_NAME, ContactDatabaseHelper.KEY_PHONENUMBER, ContactDatabaseHelper.KEY_CODE};
+        int[] to = {R.id.contactRowName, R.id.contactRowPhoneNumber, R.id.contactRowCode};
+        adapter = new SimpleCursorAdapter(this, R.layout.contact_row_layout, contactsCursor, from, to, 0);
+        contactsList.setAdapter(adapter);
+
     }
 
+    /**
+     * Function to take the values from the fields and submit it to the database.
+     *
+     * @param v the submit button
+     */
     public void onSubmitButton(View v){
         //Inserts a new contact into the database
         contact_db.insert(new Contact(name.getText().toString(), phoneNumber.getText().toString(), code.getText().toString()));
         //TODO Check for a pre-existing contact and update the information in the row instead of adding a new row
     }
 
-    //TODO Create an adapter to link data from the database to the listView (contactsList).
+    /**
+     * Function to update the listView upon the user's request.
+     *
+     * @param v the refresh button
+     */
+    public void onRefreshButton(View v) {
+        Cursor newCursor = contact_db.getContactsCursor();
+        adapter.swapCursor(newCursor);
 
+    }
+
+    /**
+     * Function to reset the table.
+     *
+     * @param v the deleteDB button
+     */
+    public void onDeleteDBButton(View v) {
+        contact_db.deleteTable();
+    }
 
 }
